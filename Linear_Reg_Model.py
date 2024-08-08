ba3059289a735e92f5a7657f780c465a0b0cda93
shaikanjum432 <shaikanjum450@gmail.com>
2024-08-08 13:51:31 +0530

    Commit the Project

---
+++ Linear_Reg_Model.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Linear_Regression():
+  
+# Initiating the parameters (learning rate and no.of iterations)
+    def __init__(self, learning_rate, no_of_iterations):
+        
+        self.learning_rate = learning_rate
+        self.no_of_iterations = no_of_iterations
+        
+    def fit(self, X, Y ):
+        
+        # number of training examples and number of features
+        self.n, self.m = X.shape # no.of rows and columns
+        
+        # initiating the weight and bias
+        self.w = np.zeros(self.m)
+        self.b = 0
+        self.X = X
+        self.Y = Y
+        
+        # Implementing Gradient Descent
+        for i in range(self.no_of_iterations):
+            self.update_weights()
+    
+    def update_weights(self):
+        
+        Y_prediction = self.predict(self.X)
+        
+        # calculate gradients
+        
+        dw = -(2 * (self.X.T).dot(self.Y - Y_prediction)) / self.n
+        
+        db = -2 * np.sum(self.Y - Y_prediction)/self.m
+        
+        # updating the weights
+        
+        self.w = self.w - self.learning_rate*dw
+        self.b = self.b - self.learning_rate*db
+    
+    def predict(self, X):
+        
+        return X.dot(self.w) + self.b
